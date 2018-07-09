e66fa688f1bffb499cd7f6474eb7a0566db4f855
Ahmad Alobaid <ahmad88me@gmail.com>
2018-07-09 11:12:56 +0200

    cleanup

--- models.go
+++ models.go
@@ -97,7 +97,6 @@ func getFromDB(collection_name string, filter *bson.Document) *list.List {
 	db := getDB()
 	collection := db.Collection(collection_name)
 	results := list.New()
-	//filter := bson.NewDocument(bson.EC.String("username", "aabcsd"))
 	cur, err := collection.Find(context.Background(), filter)
 	if err == nil {
 		defer cur.Close(context.Background())
@@ -118,9 +117,6 @@ func getFromDB(collection_name string, filter *bson.Document) *list.List {
 
 func getUserFromDB(username string) *User {
 	log.Println("get user from db")
-	// db := getDB()
-	// collection := db.Collection("user")
-	// results := list.New()
 	filter := bson.NewDocument(bson.EC.String("username", username))
 	results := getFromDB("user", filter)
 	var user *User
@@ -128,73 +124,11 @@ func getUserFromDB(username string) *User {
 	if results.Len() == 1 {
 		element := results.Front().Value.(*bson.Document)
 		log.Println(element.Lookup("username").StringValue())
-		// ele := bson.Document(*element)
-		// log.Println(reflect.TypeOf(ele))
 		user.Username = element.Lookup("username").StringValue()
 		user.Password = element.Lookup("password").StringValue()
-
-		// user.Username = element.Value.Lookup("username").StringValue()
-		// user.Password = element.Value.Lookup("password").StringValue()
 		return user
 	} else {
 		log.Printf("getUserFromDB> number of users: %d", results.Len())
 		return nil
 	}
-	// cur, err := collection.Find(context.Background(), filter)
-	// var data_m map[string]interface{}
-	// if err == nil {
-	// 	log.Println("found: ")
-	// 	defer cur.Close(context.Background())
-	// 	for cur.Next(context.Background()) {
-	// 		elem := bson.NewDocument()
-	// 		err := cur.Decode(elem)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		} else {
-	// 			log.Println("element: ")
-	// 			log.Println(elem)
-	// 			log.Println(reflect.TypeOf(elem))
-	// 			log.Println("element username: ")
-	// 			log.Println(elem.Lookup("username").StringValue())
-	// 			log.Println("printed username")
-	// 			results.PushBack(elem)
-	// 			b, err2 := elem.MarshalBSON()
-	// 			if err2 == nil {
-	// 				log.Println(b)
-	// 				log.Println("ele marshaled ok")
-	// 				err = bson.Unmarshal(b, &data_m)
-	// 				if err == nil {
-	// 					log.Println("b unmarshaled")
-	// 					log.Println(data_m)
-	// 				} else {
-	// 					log.Println("error in unmarshaled")
-	// 					log.Println(err)
-	// 				}
-	// 			} else {
-	// 				log.Println("err2")
-	// 				log.Println(err2)
-	// 			}
-	// 		}
-	// 	}
-	// 	log.Println("list: ")
-	// 	//log.Println(results.Front())
-	//
-	// 	// for temp := results.Front(); temp != nil; temp = temp.Next() {
-	// 	// 	err = temp.Value.UnmarshalBSON(&data_m)
-	// 	// 	if err == nil {
-	// 	// 		log.Println("data_m: ")
-	// 	// 		log.Println(data_m)
-	// 	// 	} else {
-	// 	// 		log.Println("error unmarshalling")
-	// 	// 	}
-	// 	//
-	// 	// 	//log.Println(temp.Value.Lookup())
-	// 	// 	//log.Println(reflect.TypeOf(temp.Value))
-	// 	// }
-	// 	log.Println("end the for")
-	// } else {
-	// 	log.Println("error: ")
-	// 	log.Println(err)
-	// }
-
 }
